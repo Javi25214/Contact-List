1314c8f33968dcecf0601fcc1df742817a7383d4
Javi25214 <javitoyrosita@gmail.com>
2023-08-10 08:11:49 +0000

    commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import "bootstrap/dist/css/bootstrap.css";
+
+
+import "../styles/index.scss";
+
+
+import Layout from "./layout";
+
+ReactDOM.render(<Layout />, document.querySelector("#app"));
